1671adb5e9562b99614cdf18f0cd4ef05bbc83d5
Virendra Patil <virendra.patil@globant.com>
2018-06-20 11:13:56 +0530

    Profile create script updated

--- server-backup/profile.sql
+++ server-backup/profile.sql
@@ -1,25 +1,21 @@
--- Table: public.profile
+-- Table: public.profiles
 
--- DROP TABLE public.profile;
+-- DROP TABLE public.profiles;
 
-CREATE TABLE public.profile
+CREATE TABLE public.profiles
 (
-    id bigint NOT NULL DEFAULT nextval('profile_id_seq'::regclass),
-    fk_app_users character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    name character varying(50) COLLATE pg_catalog."default",
-    dob character varying(20) COLLATE pg_catalog."default",
-    url_image text COLLATE pg_catalog."default",
-    occupation character varying(50) COLLATE pg_catalog."default",
-    CONSTRAINT profile_pkey PRIMARY KEY (fk_app_users),
-    CONSTRAINT profile_fk_app_users_fkey FOREIGN KEY (fk_app_users)
-        REFERENCES public.app_users (email) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
+    id integer NOT NULL DEFAULT nextval('profiles_id_seq'::regclass),
+    name character varying(255) COLLATE pg_catalog."default",
+    dob character varying(255) COLLATE pg_catalog."default",
+    url_image character varying(255) COLLATE pg_catalog."default",
+    occupation character varying(255) COLLATE pg_catalog."default",
+    "appUserEmail" character varying(255) COLLATE pg_catalog."default",
+    CONSTRAINT profiles_pkey PRIMARY KEY (id)
 )
 WITH (
     OIDS = FALSE
 )
 TABLESPACE pg_default;
 
-ALTER TABLE public.profile
+ALTER TABLE public.profiles
     OWNER to postgres;
